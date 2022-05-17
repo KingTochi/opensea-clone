d1cc0b63632a999de2efd8c6107008bbaf431100
King Tochi <tochidexing@gmail.com>
2022-05-17 04:20:29 +0100

    commit

---
+++ pages/collections/[collectionId].tsx
@@ -0,0 +1,11 @@
+import { useRouter } from 'next/router';
+import React from 'react'
+
+const Collection = () => {
+    const router = useRouter();
+  return (
+    <div>[collectionId]</div>
+  )
+}
+
+export default Collection;
