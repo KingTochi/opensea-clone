26581dd5440c984d764656a98009fc73a5402c49
King Tochi <tochidexing@gmail.com>
2022-05-18 02:07:58 +0100

    commit

---
+++ lib/sanityClient.tsx
@@ -0,0 +1,9 @@
+import sanityClient from '@sanity/client';
+
+export const client = sanityClient({
+    projectId: 'h2rfdfsv',
+    dataset: 'production',
+    apiVersion: '2021-03-25',
+    token: 'skCusd4kQmw1uu0Vk4xhGHFTGyyigAjyf5bdPOACmUzhkXYSlYGtwVpV1C10MNZ1KolPsKuVPRpJe5ZM9yLF6NQlq8TVhhidX1MJruCUpYQyYeCaMEGX3dkBmZAzXAMzGJOwVxej8aT1PBuXLMyPogGids4eNsfjAWWajzABCSIrWrkUhXiI',
+    useCdn: false,
+})
